d051abde24c5b0b5f4bf93d5addba42d28d4b781
Galatenea98 <elizabethtenayucra@gmail.com>
2020-10-31 06:55:08 -0500

    Feature de 4 historias de usuario

---
+++ testing-bdd/src/test/java/FiltrarPreciosSteps.java
@@ -0,0 +1,43 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FiltrarPreciosSteps {
+
+    @Given("el cliente quiere buscar un servicio de acuerdo a su presupuesto")
+    public void elClienteQuiereBuscarUnServicioDeAcuerdoASuPresupuesto() {
+    }
+
+    @When("busque un servicio determinado")
+    public void busqueUnServicioDeterminado() {
+    }
+
+    @Then("deberá elegir la opción de Filtrar precios en el buscador del aplicativo")
+    public void deberaElegirLaOpcionDeFiltrarPreciosEnElBuscadorDelAplicativo() {
+    }
+
+    @Given("el cliente ha filtrado los servicios por el precio")
+    public void elClienteHaFiltradoLosServiciosPorElPrecio() {
+    }
+
+    @When("elige la opción de Filtrar precios en el buscador del aplicativo")
+    public void eligeLaOpcionDeFiltrarPreciosEnElBuscadorDelAplicativo() {
+    }
+
+    @Then("se muestra una vista con todos los servicios con el precio de acuerdo al presupuesto")
+    public void seMuestraUnaVistaConTodosLosServiciosConElPrecioDeAcuerdoAlPresupuesto() {
+    }
+
+    @Given("el cliente visualiza los servicios de acuerdo al presupuesto")
+    public void elClienteVisualizaLosServiciosDeAcuerdoAlPresupuesto() {
+    }
+
+    @When("selecciona el servicio requerido")
+    public void seleccionaElServicioRequerido() {
+    }
+
+    @Then("podrá gestionar la solicitud de reserva")
+    public void podraGestionarLaSolicitudDeReserva() {
+    }
+
+}
